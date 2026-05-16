ffdb453940fd6daa1ea92f735c7cce415bb4ed23
Prem kishor <premkishor131@gmail.com>
2026-05-16 17:39:18 +0530

    Time: 24 ms (60.65%), Space: 3.9 MB (85.15%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0195-tenth-line/0195-tenth-line.sh
@@ -0,0 +1,6 @@
+# Read from the file file.txt and output the tenth line to stdout.
+sed -n '10p' file.txt
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
