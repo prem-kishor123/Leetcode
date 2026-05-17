4f7c346a150079cacf1cbb22f2a4855cb55073be
Prem kishor <premkishor131@gmail.com>
2026-05-17 21:54:38 +0530

    Time: 1 ms (82.79%), Space: 42.7 MB (72.71%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+        
+        if (pattern.length() != words.length) {
+            return false;
+        }
+
+        java.util.HashMap<Character, String> map1 = new java.util.HashMap<>();
+        java.util.HashMap<String, Character> map2 = new java.util.HashMap<>();
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char ch = pattern.charAt(i);
+            String word = words[i];
+
+            if (map1.containsKey(ch)) {
+                if (!map1.get(ch).equals(word)) {
+                    return false;
+                }
+            } else {
+                map1.put(ch, word);
+            }
+
+            if (map2.containsKey(word)) {
+                if (map2.get(word) != ch) {
+                    return false;
+                }
+            } else {
+                map2.put(word, ch);
+            }
+        }
+
+        return true;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
