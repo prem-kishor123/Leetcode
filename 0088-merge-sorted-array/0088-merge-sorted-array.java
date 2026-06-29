8d7d00f10c9ac480c3e32f359c87fcb5146f4a42
Prem kishor <premkishor131@gmail.com>
2026-06-29 22:48:07 +0530

    Time: 4 ms (16.17%), Space: 44 MB (15.02%) - LeetHub - Time: O((m+n)log(m+n)), Space: O(1)

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+    for(int i=0;i<n ; i++){
+        nums1[m+i] =nums2[i];
+    }
+    Arrays.sort(nums1);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
