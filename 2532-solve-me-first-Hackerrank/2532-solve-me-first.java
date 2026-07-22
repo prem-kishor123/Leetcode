adc400ead5dd550b91fe40c65afd9eff1dc33c44
Prem kishor <premkishor131@gmail.com>
2026-07-22 14:44:27 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 2532-solve-me-first-Hackerrank/2532-solve-me-first.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Solution {
+
+    static int solveMeFirst(int a, int b) {
+      // Hint: Type return a+b; below 
+      
+      return a+b;
+	}
+
+  
+   public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a;
+        a = in.nextInt();
+        int b;
+        b = in.nextInt();
+        in.close();
+        int sum;
+        sum = solveMeFirst(a, b);
+        System.out.println(sum);
+	}
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
